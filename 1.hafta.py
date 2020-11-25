a457a2afe643c5019c8c94fe99a3e39e6a85dcc3
baranselcilinger <baranselciliinger@gmail.com>
2020-11-25 12:58:19 +0300

    Örnekler ile ekran çıktısı almak

--- 1.hafta.py
+++ 1.hafta.py
@@ -1,2 +1,34 @@
 print("Hello world")
 #Ekran çıktısı almak için kullanılır
+
+#Farklı örnekler ile ekran çıktıları
+ad = "Baransel Çilinger"
+print(ad)
+sayi=35
+Sayi=45
+print(sayi)
+
+
+#type değerin hangi türden olduğunu yazdırmak için kullanılır.
+print("ad değişkenin türü:",type(ad))
+print("sayi değişkenin türü:",type(sayi))
+
+#upper yazdırılıcak olan harflerin büyük yazılmasını sağlar.
+harfler="abcdef" #string
+print(harfler.upper())
+print("abcdef".upper())
+
+#Kullanıcıdan alıcağımız ad-soyad bilgisi ile ekrana yazdırma işlemi yaptık.
+Ad=input("Lütfen Adınızı Giriniz:")
+Soyad=input("Soyadınızı Giriniz:")
+print("Merhaba",Ad, Soyad)
+print("Merhaba {} {}".format(Ad,Soyad))
+
+
+#input değerini str olarak girsek bile int'e dönüştürme işlemi yaptığımız
+#için programımız hata vericektir
+sayi1=int(input("Sayı giriniz:"))#str 20
+sayi2=int(input("Sayı giriniz:"))#str 10
+
+toplam=sayi1+sayi2
+print(toplam)
